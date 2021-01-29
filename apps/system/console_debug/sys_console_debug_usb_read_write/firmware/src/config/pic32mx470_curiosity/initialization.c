9c6e37d31c31cf1e94c0bdb33d4845c93cf574bb
Amit Raddi <amit.raddi@microchip.com>
2021-01-29 15:47:50 +0530

    [SYS_CONSOLE_DEBUG] Regenerated demo for system_console_debug_sys_console_debug_usb_read_write_pic32mx470_curiosity
    MH3-49876
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/system/console_debug/sys_console_debug_usb_read_write/firmware/src/config/pic32mx470_curiosity/initialization.c
+++ apps/system/console_debug/sys_console_debug_usb_read_write/firmware/src/config/pic32mx470_curiosity/initialization.c
@@ -264,6 +264,7 @@ const SYS_CONSOLE_INIT sysConsole0Init =
 
 void SYS_Initialize ( void* data )
 {
+
     /* Start out with interrupts disabled before configuring any modules */
     __builtin_disable_interrupts();
 
