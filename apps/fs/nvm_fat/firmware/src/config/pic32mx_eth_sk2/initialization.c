3da3cdf3179f45d94b37e148293320b7f32e193f
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2022-05-27 00:47:37 +0530

    [SYS_FS] Regenerated demo for fs_nvm_fat_pic32mx_eth_sk2
    MH3-64716
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/fs/nvm_fat/firmware/src/config/pic32mx_eth_sk2/initialization.c
+++ apps/fs/nvm_fat/firmware/src/config/pic32mx_eth_sk2/initialization.c
@@ -94,6 +94,7 @@
 
 
 
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: Driver Initialization Data
@@ -252,8 +253,8 @@ void SYS_Initialize ( void* data )
     CHECONbits.PFMWS = 2;
     CHECONbits.PREFEN = 3;
 
-    /* Set the SRAM wait states to zero */
-    BMXCONbits.BMXWSDRM = 0;
+    /* Set the SRAM wait states to One */
+    BMXCONbits.BMXWSDRM = 1;
 
     /* Configure Debug Data Port */
     DDPCONbits.JTAGEN = 0;
