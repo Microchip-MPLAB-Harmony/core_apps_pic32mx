08e7b6a52e873bfa8bad1d6e659e341fd3a73b31
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2022-05-27 01:29:24 +0530

    [SYS_TIME] Regenerated demo for system_time_sys_time_multiclient_pic32mx_eth_sk2
    MH3-64731
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/system/time/sys_time_multiclient/firmware/src/config/pic32mx_eth_sk2/definitions.h
+++ apps/system/time/sys_time_multiclient/firmware/src/config/pic32mx_eth_sk2/definitions.h
@@ -55,10 +55,10 @@
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/gpio/plib_gpio.h"
 #include "peripheral/evic/plib_evic.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
--- apps/system/time/sys_time_multiclient/firmware/src/config/pic32mx_eth_sk2/initialization.c
+++ apps/system/time/sys_time_multiclient/firmware/src/config/pic32mx_eth_sk2/initialization.c
@@ -94,6 +94,7 @@
 
 
 
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: Driver Initialization Data
@@ -200,8 +201,8 @@ void SYS_Initialize ( void* data )
     CHECONbits.PFMWS = 2;
     CHECONbits.PREFEN = 3;
 
-    /* Set the SRAM wait states to zero */
-    BMXCONbits.BMXWSDRM = 0;
+    /* Set the SRAM wait states to One */
+    BMXCONbits.BMXWSDRM = 1;
 
     /* Configure Debug Data Port */
     DDPCONbits.JTAGEN = 0;
