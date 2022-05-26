320b589b3dd9d5e61e53b7a9733bb9a728e2eaf3
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2022-05-27 01:10:26 +0530

    [FREERTOS] Regenerated demo for rtos_freertos_basic_freertos_pic32mx_xlp_sk
    MH3-64724
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/rtos/freertos/basic_freertos/firmware/src/config/pic32mx_xlp_sk/initialization.c
+++ apps/rtos/freertos/basic_freertos/firmware/src/config/pic32mx_xlp_sk/initialization.c
@@ -105,6 +105,7 @@
 
 
 
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: Driver Initialization Data
@@ -167,8 +168,8 @@ void SYS_Initialize ( void* data )
     CHECONbits.PFMWS = 3;
     CHECONbits.PREFEN = 3;
 
-    /* Set the SRAM wait states to zero */
-    BMXCONbits.BMXWSDRM = 0;
+    /* Set the SRAM wait states to One */
+    BMXCONbits.BMXWSDRM = 1;
 
 
 
