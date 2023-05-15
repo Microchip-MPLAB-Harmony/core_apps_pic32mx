f6791bb7e206cdfafa1661d6a124e93af925f077
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-05-15 18:16:52 +0530

    [SYS_TIME] Regenerated demo for system_time_sys_time_multiclient_pic32mx470_curiosity
    MH3-76188

--- apps/system/time/sys_time_multiclient/firmware/src/config/pic32mx470_curiosity/definitions.h
+++ apps/system/time/sys_time_multiclient/firmware/src/config/pic32mx470_curiosity/definitions.h
@@ -55,10 +55,10 @@
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/gpio/plib_gpio.h"
 #include "peripheral/evic/plib_evic.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
@@ -70,6 +70,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "PIC32MX470F512H"
+#define DEVICE_ARCH			 "MIPS"
+#define DEVICE_FAMILY		 "PIC32MX1185"
+#define DEVICE_SERIES		 "PIC32MX"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 96000000
