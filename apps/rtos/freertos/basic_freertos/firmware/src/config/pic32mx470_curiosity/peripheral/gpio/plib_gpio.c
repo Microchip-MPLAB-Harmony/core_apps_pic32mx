fb1eeee0a861999f478abd4c259864495eb2b272
Syam Nair <Syam.Nair@microchip.com>
2025-09-23 10:02:21 +0000

    [APP] Regenerated demo for rtos__freertos__basic_freertos__firmware
    MH3C-331

--- apps/rtos/freertos/basic_freertos/firmware/src/config/pic32mx470_curiosity/peripheral/gpio/plib_gpio.c
+++ apps/rtos/freertos/basic_freertos/firmware/src/config/pic32mx470_curiosity/peripheral/gpio/plib_gpio.c
@@ -46,10 +46,10 @@
 
 
 /* Array to store callback objects of each configured interrupt */
-volatile static GPIO_PIN_CALLBACK_OBJ portPinCbObj[1];
+static volatile GPIO_PIN_CALLBACK_OBJ portPinCbObj[1];
 
 /* Array to store number of interrupts in each PORT Channel + previous interrupt count */
-volatile static uint8_t portNumCb[6 + 1] = { 0, 0, 0, 1, 1, 1, 1, };
+static volatile uint8_t portNumCb[6 + 1] = { 0, 0, 0, 1, 1, 1, 1, };
 
 /******************************************************************************
   Function:
