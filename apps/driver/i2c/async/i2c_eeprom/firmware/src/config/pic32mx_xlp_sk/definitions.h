543550c615fd844cf8a02b90c35f7106c484cf6f
Syam Nair <Syam.Nair@microchip.com>
2025-09-23 10:08:03 +0000

    [APP] Regenerated demo for driver__i2c__async__i2c_eeprom__firmware
    MH3C-331

--- apps/driver/i2c/async/i2c_eeprom/firmware/src/config/pic32mx_xlp_sk/definitions.h
+++ apps/driver/i2c/async/i2c_eeprom/firmware/src/config/pic32mx_xlp_sk/definitions.h
@@ -14,7 +14,7 @@
 
 //DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -53,10 +53,10 @@
 #include "peripheral/gpio/plib_gpio.h"
 #include "peripheral/evic/plib_evic.h"
 #include "bsp/bsp.h"
+#include "driver/i2c/drv_i2c.h"
 #include "system/int/sys_int.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "driver/i2c/drv_i2c.h"
 #include "app.h"
 
 
